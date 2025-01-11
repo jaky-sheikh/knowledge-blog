dbd8321608935a82a28226e479a9192263aba574
jaky-sheikh <jaky@eu4m.eu>
2025-01-11 14:01:43 +0600

    display blog data and hashtag

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,9 +1,27 @@
 import PropTypes from 'prop-types';
 const Blog = ({ blog }) => {
-    console.log(blog);
+    const { title, cover, author, author_img, posted_date, reading_time, hashtags } = blog;
     return (
         <div>
-
+            <img src={cover} alt={`Cover picture of the title ${title}`} />
+            <div className='flex justify-between'>
+                <div className='flex gap-2'>
+                    <img className='w-14' src={author_img} alt="" />
+                    <div>
+                        <h3 className='text-2xl'>{author}</h3>
+                        <p>{posted_date}</p>
+                    </div>
+                </div>
+                <div>
+                    <span>{reading_time} min read</span>
+                </div>
+            </div>
+            <h2 className='text-3xl'>{title}</h2>
+            <p>
+                {
+                    hashtags.map((hash, idx) => <span key={idx}><a href="">#{hash}</a></span>)
+                }
+            </p>
         </div>
     );
 };
